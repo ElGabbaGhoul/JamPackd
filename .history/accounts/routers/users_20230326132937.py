2a41adc17f619993739b2620ba0babcaa1e762ac
Marble <scnideffer@gmail.com>
2023-03-26 14:57:52 -0700

    working on backend, fastapi. module not found error from main.py line 2.

---
+++ .history/accounts/routers/users_20230326132937.py
@@ -0,0 +1,92 @@
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+    Request,
+    HTTPException,
+    status,
+)
+from pydantic import BaseModel
+from authenticator import authenticator
+from jwtdown_fastapi.authentication import Token
+from queries.users import (
+    UserIn,
+    UserOut,
+    DuplicateAccountError,
+    UserQueries,
+)
+
+
+router = APIRouter()
+
+
+class AccountForm(BaseModel):
+    username: str
+    password: str
+
+
+class AccountToken(Token):
+    account: UserOut
+
+
+class HttpError(BaseModel):
+    detail: str
+
+
+@router.get("/api/users/{user_id}", response_model=UserOut)
+def get_user(
+    username: str,
+    response: Response,
+    queries: UserQueries = Depends(),
+):
+    record = queries.get_user(username)
+    if record is None:
+        response.status_code = 404
+    else:
+        return record
+
+
+@router.post("/api/accounts", response_model=AccountToken | HttpError)
+async def create_user(
+    info: UserIn,
+    request: Request,
+    response: Response,
+    accounts: UserQueries = Depends(),
+):
+    hashed_password = authenticator.hash_password(info.password)
+    try:
+        account = accounts.create_user(info, hashed_password)
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot create an account with those credentials",
+        )
+    form = AccountForm(username=info.username, password=info.password)
+    token = await authenticator.login(response, request, form, accounts)
+    return AccountToken(account=account, **token.dict())
+
+
+@router.delete("/api/accounts/{user_id}", response_model=bool)
+def delete_user(user_id: str, queries: UserQueries = Depends()):
+    queries.delete_user(user_id)
+    return True
+
+
+@router.get("/api/accounts/me/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: dict = Depends(authenticator.try_get_current_account_data),
+) -> AccountToken | None:
+
+    # example of when you might want authenticator.try_get_current_account_data
+    # if account:
+    #     # logged in response
+    # else:
+    #     # non logged in response
+
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
---
+++ accounts/main.py
@@ -0,0 +1,48 @@
+import os
+from routers import users
+
+# from queries.users import UserIn
+from authenticator import authenticator
+from fastapi import FastAPI, Request
+
+# from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    os.environ.get("CORS_HOST", "REACT_APP_API_HOST"),
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8001",
+    "http://localhost:8003",
+    "http://localhost:8081"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(users.router)
+app.include_router(authenticator.router)
+
+
+@app.get("/")
+async def read_root(request: Request):
+    print(request)
+    return {}
+
+
+# import UserIn
+@app.put("/users/{id}")
+async def remove_hashing_password(UserIn):
+    data = UserIn
+    data.pop("hashed_password")
